ecd4bf54cd3a5b0a2a32589ce2d0b14dd7ec0eae
Aishwarya Yeole <aishwarya.yeole@xoriant.com>
2018-08-13 19:00:15 +0530

    successful axios call-2

--- src/AsyncComponentLoader.js
+++ src/AsyncComponentLoader.js
@@ -19,10 +19,9 @@ export default class AsyncComponentLoader extends Component {
     constructor(props, context) {
         super();
         this.state = {
-            //   hideFooter: false,
-            //   hideNavBar: true
+        
         }
-        // this.viewHanlder = this.viewHanlder.bind(this);
+        this.viewHanlder = this.viewHanlder.bind(this);
     }
 
       viewHanlder(event) {
@@ -30,14 +29,7 @@ export default class AsyncComponentLoader extends Component {
             hashHistory.push("/");
       }
 
-    //   loginViewHandler(hideFooterFlag, hideNavBarFlag, event) {
-    //     if ((event.components == undefined)){
-    //       this.setState({
-    //         hideFooter: hideFooterFlag,
-    //         hideNavBar: hideNavBarFlag
-    //       });
-    //     }
-    //   }
+  
 
     render() {
         return (
@@ -45,22 +37,6 @@ export default class AsyncComponentLoader extends Component {
                 <App >
                     <Router history={hashHistory}>
                         <Route path="/" onEnter={this.viewHanlder.bind(this)} component={AsyncMemesGenerator} />
-
-                        {/* <Route path="accessRequest" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncAccessRequest} />
-                        <Route path="profile" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncProfile} />
-                        <Route path="approverMapping" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncApproverMapping} />
-                        <Route path="mappedapprover" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncMappedApprover} />
-                        <Route path="userManagement" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncUserManagement} />
-                        <Route path="approverAccessRequest" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncApproverAccessRequest} />
-                        <Route path="resetPassword" onEnter={this.viewHanlder.bind(this, false, true)} component={AsyncResetPassword} />
-                        <Route path="managerBackup" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncManagerBackup} />
-                        <Route path="managerAccessRequest" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncManagerAccessRequest} />
-                        <Route path="userRequest" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncUserRequest} />
-                        <Route path="pendingRequest" onEnter={this.loginViewHandler.bind(this, false, true)} component={AsyncPendingRequest} />
-                        <Route path="pendingUserRequest" onEnter={this.loginViewHandler.bind(this, false, true)} component={AsyncPendingUserRequest} />
-                        <Route path="applicationOwnerMapping" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncApplicationOwnerMapping} />
-                        <Route path="mappedapplicationOwner" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncMappedApplicationOwner} />
-                        <Route path="unauthorizedUser" onEnter={this.viewHanlder.bind(this, true, false)} component={AsyncUnauthorizedUserPage} /> */}
                     </Router>
                 </App>
             </div>)
